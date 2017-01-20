693dadd0fa852c0aefe855acfa17829723583c51
joshua <joshua.deguzman@searchoptmedia.com>
2017-01-20 12:17:30 +0800

    fixing cannot login

--- web/src/CoreBundle/Model/EmpTimePeer.php
+++ web/src/CoreBundle/Model/EmpTimePeer.php
@@ -10,8 +10,8 @@ class EmpTimePeer extends BaseEmpTimePeer{
 	public static function getTime($id, Criteria $c = null){
 		if (is_null($c)) {
 			$c = new Criteria();
-		}	
-
+		}
+		
 		$c->add(self::EMP_ACC_ACC_ID, $id, Criteria::EQUAL);
 
 		$_self = self::doSelect($c);
