f7e87c70672d605c7325e1e1bca77132cb33ba1c
jarnielcataluna <jarniel.cataluna@searchoptmedia.com>
2017-04-26 16:35:13 +0800

    Add new utility - CONSTANT

---
+++ web/src/CoreBundle/Utilities/Constant.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace CoreBundle\Utilities;
+
+class Constant
+{
+    const EVENT_TYPE_HOLIDAY        =   1;
+    const EVENT_TYPE_INTERNAL       =   2;
+    const EVENT_TYPE_MEETING        =   3;
+}
