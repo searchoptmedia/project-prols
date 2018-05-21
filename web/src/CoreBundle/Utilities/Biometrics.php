04a107609bf02bd3c6e13a6ba43b1f279c4c8019
Jorge Viray <jorge.viray@searchoptmedia.com>
2018-05-21 14:22:33 +0800

    created base logic on biometrics and its own class.

---
+++ web/src/CoreBundle/Utilities/Biometrics.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace CoreBundle\Utilities;
+
+use CoreBundle\Model\BiometricPendingLogsQuery;
+use CoreBundle\Model\BiometricProcessedLogs;
+use CoreBundle\Model\BiometricProcessedLogsQuery;
+use CoreBundle\Model\EmpProfileQuery;
+use CoreBundle\Model\EmpTime;
+use CoreBundle\Model\EmpTimeQuery;
+
+class Biometrics
+{
+	protected $_biometricProcessedLogs;
+	protected $_biometricPendingLogs;
+
+	public function __construct()
+	{
+		$this->_biometricProcessedLogs = BiometricProcessedLogsQuery::create();
+		$this->_biometricPendingLogs = BiometricPendingLogsQuery::create()->toArray();
+	}
+
+	private function decodeDate($datestring = "")
+	{
+		$datestringArray = str_split($datestring,2);
+		return
+			array(
+				'month' => (int)$datestringArray[0],
+				'day' => (int)$datestringArray[1],
+				'year' => (int)$datestringArray[2]+2000
+			);
+	}
+
+	private function decodeTime($timeinteger = "")
+	{
+		$timestringArray = str_split($timeinteger,2);
+		return
+			array(
+				'hour' => (int)$timestringArray[0],
+				'minute' => (int)$timestringArray[1],
+				'second' => (int)$timestringArray[2]
+			);
+	}
+
+	private function decodeEmp($employeeNumber = "")
+	{
+		$employeeNumberArray = explode('-',$employeeNumber);
+		$processedEmployeeNumber = $employeeNumberArray[0].$employeeNumberArray[1];
+		$currentEmployee = EmpProfileQuery::create()
+			->findOneByEmployeeNumber($processedEmployeeNumber);
+		return $currentEmployee;
+	}
+
+
+	public function updateLogs()
+	{
+		if(count($this->_biometricPendingLogs) != 0)
+		{
+			$pendingLogIds = array();
+			foreach($this->_biometricPendingLogs as $pendingLog)
+			{
+				$logDate = $this->decodeDate($pendingLog['C_Date']);
+				$logTime = $this->decodeTime($pendingLog['C_Time']);
+				$employee = $this->decodeEmp($pendingLog['L_UID']);
+				if($employee != null)
+				{
+					$processedLogId = 0;
+					$newDateTime = date('m/d/Y h:i:s a', time());
+					$newDateTime = date_date_set(
+						$newDateTime,
+						$logDate['year'],
+						$logDate['month'],
+						$logDate['day']);
+					$currentDate = $newDateTime;
+					$newDateTime = date_time_set(
+						$newDateTime,
+						$logTime['hour'],
+						$logTime['minute'],
+						$logTime['second']);
+					$noTimeoutLog = EmpTimeQuery::create()
+						->filterByDate($currentDate)
+						->filterByEmpAccAccId($employee->getEmpAccAccId())
+						->findOneByTimeOut(NULL,\Criteria::EQUAL);
+					if($noTimeoutLog == null)
+					{
+						$freshLog = new EmpTime();
+						$freshLog
+							->setEmpAccAccId($employee->getEmpAccAccId())
+							->setDate($currentDate)
+							->setTimeIn($newDateTime)
+							->save();
+						$processedLogId = $freshLog
+							->getId();
+					}
+					else
+					{
+						$noTimeoutLog
+							->setTimeOut($newDateTime);
+						$manhours = date_diff(new \DateTime($newDateTime), new \DateTime($noTimeoutLog));
+						$h = $manhours->format('%h');
+						$i = intval($manhours->format('%i'));
+						$i = $i > 0 ? ($i / 60) : 0;
+						$totalHoursDec = number_format($h + $i, 2);
+
+						$overtime = 0;
+						if ($totalHoursDec > 9) {
+							$overtime = $totalHoursDec - 9;
+						}
+
+						$noTimeoutLog->setOvertime($overtime);
+						$noTimeoutLog->setManhours($totalHoursDec);
+						$noTimeoutLog->save();
+						$processedLogId = $noTimeoutLog->getId();
+					}
+					$processedLog = new BiometricProcessedLogs();
+					$processedLog
+						->setC_Date($logDate)
+						->setC_Time($logTime)
+						->setemp_time_id($processedLogId)
+						->save();
+					array_push($pendingLogIds,$pendingLog['id']);
+				}
+			}
+			BiometricPendingLogsQuery::create()
+				->filterByid($pendingLogIds)
+				->delete();
+		}
+	}
+
+
+}
